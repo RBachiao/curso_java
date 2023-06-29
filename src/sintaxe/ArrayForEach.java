67f36d02d357e698d450289e7b4f51e8bd3f6b7f
RBachiao <133903659+RBachiao@users.noreply.github.com>
2023-06-29 16:23:36 -0300

    Update and rename ArrayForr.java to ArrayForEach.java

---
+++ src/sintaxe/ArrayForEach.java
@@ -0,0 +1,21 @@
+package revisao;
+
+public class ExemploArray {
+
+	public static void main(String[] args) {
+		// tudoPar é um array, nesse caso um conjunto de inteiros
+		int[] tudoPar = new int[5];
+		
+		for ( int i = 0; i < tudoPar.length; i ++ ) {
+
+			tudoPar[i] = 2;
+		}
+		// para cada elemento de tudoPar faça valor receber esse elemento e, nesse caso, imprima o valor do elemento
+		// valor recebe o elemento de cada posição cada vez que roda o for
+		for ( int valor : tudoPar ) {
+		
+			System.out.print(valor + " - ");
+	
+		}
+	}
+}
