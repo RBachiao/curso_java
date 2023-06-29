0a30250143234c4f58e8770fba4556cfc663c02b
RBachiao <133903659+RBachiao@users.noreply.github.com>
2023-06-29 14:26:31 -0300

    Update Arrays.java

--- src/sintaxe/Arrays.java
+++ src/sintaxe/Arrays.java
@@ -9,16 +9,20 @@ public class ExemploArray {
 		// meuBuque é um array [] do tipo String
 		// pode ter notação String meuBuque[]
 		// contrutor é um array do tipo String com tamanho de 6 posições [6]
-		String[] meuBuque = new String[6];
+		String[] meuBuque = new String[5];
 		// meuBuque indice 0 é Rosa
 		meuBuque[0] = "Rosa";
 		meuBuque[1] = "Girassol";
 		meuBuque[2] = "Margarida";
 		meuBuque[3] = "Jasmim";
 		meuBuque[4] = "Cravo";
-		meuBuque[5] = "Violeta";
 		
 		System.out.println("Meu buque na posição 2 é: " + meuBuque[2]);
+		
+		// ou
+		String[] meuJardim = { "Lirio", "Crisantemo", "Dama da Noite", "Tulipa", "Violeta" };
+		
+		System.out.println("Meu jardim na posição 2 é: " + meuJardim[2]);
 		
 
 	}
