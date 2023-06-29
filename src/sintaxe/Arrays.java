6b8ce24e911e48ea8474721c0e76d66f9f36414d
RBachiao <133903659+RBachiao@users.noreply.github.com>
2023-06-29 14:15:39 -0300

    Create Arrays.java

---
+++ src/sintaxe/Arrays.java
@@ -0,0 +1,26 @@
+package revisao;
+
+public class ExemploArray {
+
+	public static void main(String[] args) {
+		// array de tipo primitivo não tem método. Contém apenas o valor que coloquei
+		// o array é sempre um objeto]
+		// [ ] identifica que é um array
+		// meuBuque é um array [] do tipo String
+		// pode ter notação String meuBuque[]
+		// contrutor é um array do tipo String com tamanho de 6 posições [6]
+		String[] meuBuque = new String[6];
+		// meuBuque indice 0 é Rosa
+		meuBuque[0] = "Rosa";
+		meuBuque[1] = "Girassol";
+		meuBuque[2] = "Margarida";
+		meuBuque[3] = "Jasmim";
+		meuBuque[4] = "Cravo";
+		meuBuque[5] = "Violeta";
+		
+		System.out.println("Meu buque na posição 2 é: " + meuBuque[2]);
+		
+
+	}
+
+}
