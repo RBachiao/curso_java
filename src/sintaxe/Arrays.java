c321445546dc45217d34316a10a83cb85668e34f
RBachiao <133903659+RBachiao@users.noreply.github.com>
2023-06-29 15:21:51 -0300

    Update Arrays.java

--- src/sintaxe/Arrays.java
+++ src/sintaxe/Arrays.java
@@ -17,14 +17,33 @@ public class ExemploArray {
 		meuBuque[3] = "Jasmim";
 		meuBuque[4] = "Cravo";
 		
-		System.out.println("Meu buque na posição 2 é: " + meuBuque[2]);
+		System.out.println("Meu buque na 3ª posição é: " + meuBuque[2]);
 		
 		// ou
 		String[] meuJardim = { "Lirio", "Crisantemo", "Dama da Noite", "Tulipa", "Violeta" };
 		
-		System.out.println("Meu jardim na posição 2 é: " + meuJardim[2]);
+		System.out.println("Meu jardim na 3ª posição é: " + meuJardim[2]);
 		
-
+		int[] meusNumeros = new int[4];
+		meusNumeros[0] = 1;
+		meusNumeros[1] = 2;
+		meusNumeros[2] = 3;
+		meusNumeros[3] = 4;
+		
+		System.out.println("Meu número na 3ª posição é: " + meusNumeros[2]);
+		
+		double[] meusNum = new double[3];
+		meusNum[0] = 1;
+		meusNum[1] = 2;		
+		
+		System.out.println("Meu número na 1ª posição é " + meusNum[0] + " e na 3ª posição é " + meusNum[2]);
+		
+		boolean[] meusTf = new boolean[3];
+		meusTf[1] = true;
+		meusTf[2] = false;		
+		
+		System.out.println("Meu boleano na 1ª posição é " + meusTf[0] + " e na 2ª posição é " + meusTf[1] + " e na 3ª posição é " + meusTf[2]);
+		
 	}
 
 }
