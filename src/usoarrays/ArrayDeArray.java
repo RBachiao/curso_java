faef2ca64566e798bca8dcfd22fd0bfc132a6641
RBachiao <133903659+RBachiao@users.noreply.github.com>
2023-07-18 14:33:19 -0300

    Update ArrayDeArray.java

--- src/usoarrays/ArrayDeArray.java
+++ src/usoarrays/ArrayDeArray.java
@@ -1,15 +1,38 @@
 package usoarrays;
 
+import java.util.Scanner;
+
 public class ArrayDeArray {
 
 	public static void main(String[] args) {
+
+		//o arranjo/array será de 1 array com 3 arrays com 2 posições/elementos
+		//3 linhas e 2 colunas
+		Scanner entrada = new Scanner(System.in);
 		
-		//o arranjo/array será de 1 array com 2 arrays com 3 posições/elementos
-		int[][] meuArrei = { { 1,2,3 }, { 4,5,6 } };
+		int[][] meuArrei = new int[3][2];
 		
 		// ou 
-		// meuArrei = new int[2][3];
+		// meuArrei = new int[3][2];
 		
-			System.out.print(meuArrei[1][0]);
+		for (int i = 0; i < meuArrei.length; i++) {
+			
+			for (int j = 0; j < meuArrei[i].length; j++) {
+				
+				System.out.println("Entre com um valor para a " +  (i+1) + "ª linha e " +
+						(j+1) + "ª coluna: ");
+				
+				meuArrei[i][j] = entrada.nextInt();
+			}
+		} entrada.close();
+
+		for (int i = 0; i < meuArrei.length; i++) {
+			
+			for (int j = 0; j < meuArrei[i].length; j++) {
+				
+				System.out.print(meuArrei[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
